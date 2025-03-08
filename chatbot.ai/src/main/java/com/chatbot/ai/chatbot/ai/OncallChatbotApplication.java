80a49f87f60d37af47f555e2eb48803ce8218cbf
Akash Sidhue <akashsidhue@gmail.com>
2025-03-08 13:42:53 +0530

    Initial commit - Spring Boot project

---
+++ chatbot.ai/src/main/java/com/chatbot/ai/chatbot/ai/OncallChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.ai.chatbot.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncallChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OncallChatbotApplication.class, args);
+	}
+
+}
---
+++ chatbot.ai/src/test/java/com/chatbot/ai/chatbot/ai/OncallChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.ai.chatbot.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncallChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
